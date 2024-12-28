b3d812d53de66c6249bfc66e3216469bec21d84b
Mohamed Elmalky <mohamed.elmalky@kaizen.io>
2024-12-28 11:22:37 -0600

    refactor ToEvent to use mediatr instead

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections.Concurrent;
+using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 Console.WriteLine("Hello World!");
 
-var addCustomer = new AddCustomer(GenerateIds.NewId());
-var customerAdded = addCustomer.ToEvent();
-Console.WriteLine(customerAdded.Kind);
+using IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(static (context, services) =>
+    {
+        services.AddMediatR(cfg => 
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
+        );
+    })
+    .Build();
 
+using var scope = host.Services.CreateScope();
+var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+var addCustomerCommand = new AddCustomerCommand(GenerateIds.NewId());
+var customerAdded = await mediator.Send(addCustomerCommand);
+Console.WriteLine($"Customer added: {customerAdded.AggregateId}");
 
 Console.WriteLine("Press any key to exit...");
 Console.ReadLine();
@@ -15,16 +30,34 @@ public abstract record Event()
     public abstract string Kind { get; }
     public abstract string AggregateId { get; init; }
 }
-public abstract record Command()
+
+public record CustomerAdded(string AggregateId) : Event
 {
-    public abstract Event ToEvent();
-};
+    public override string Kind => "customer-added";
+}
+
+public class AddCustomerCommand : IRequest<CustomerAdded>
+{
+    public string AggregateId { get; init; }
+    public AddCustomerCommand(string aggregateId) => AggregateId = aggregateId;
+}
+
+public class AddCustomerCommandHandler : IRequestHandler<AddCustomerCommand, CustomerAdded>
+{
+    public Task<CustomerAdded> Handle(AddCustomerCommand request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(new CustomerAdded(request.AggregateId));
+    }
+}
+
+
 public interface IEventStore
 {
     public List<Event> Events { get; }
     Task Append(string aggregateType, Event e);
     Task Load();
 }
+
 public class MemoryEventStore : IEventStore
 {
     readonly ConcurrentDictionary<string, List<Event>> _store = new();
@@ -43,100 +76,9 @@ public class MemoryEventStore : IEventStore
         throw new NotImplementedException();
     }
 }
+
 public static class GenerateIds
 {
     private static int _id = 1000;
     public static string NewId() => (++_id).ToString();
-}
-
-public record AddCustomer(string AggregateId) : Command
-{
-    public override Event ToEvent() => new CustomerAdded(AggregateId);
-}
-
-public record CustomerAdded(string AggregateId) : Event
-{
-    public override string Kind => "customer-added";
-}
-
-public class MemoryQueue
-{
-    private readonly ConcurrentQueue<(TaskCompletionSource tsc, Command cmd)> _queue = new();
-
-    public Task Enqueue(Command item)
-    {
-        var tcs = new TaskCompletionSource();
-        _queue.Enqueue((tcs, item));
-        return tcs.Task;
-    }
-
-    public IEnumerable<(TaskCompletionSource, Command)> Dequeue()
-    {
-        var cmd = _queue.TryDequeue(out var item);
-        if (!cmd)
-            yield break;
-        yield return item;
-    }
-
-    public bool IsEmpty => _queue.IsEmpty;
-}
-
-public class CommandHandler
-{
-    readonly MemoryQueue _queue;
-    readonly IEventStore _eventStore;
-    const int EXCEPTION_DELAY = 5000;
-    const int POLL_DELAY = 100;
-
-    public CommandHandler(MemoryQueue queue, IEventStore eventStore)
-    {
-        _queue = queue;
-        _eventStore = eventStore;
-    }
-
-    public async Task Run(CancellationToken token)
-    {
-        try
-        {
-            while (!token.IsCancellationRequested)
-            {
-                await HandleQueue();
-                if (_queue.IsEmpty)
-                    await Task.Delay(POLL_DELAY, token);
-            }
-        }
-        catch (OperationCanceledException) { }
-        finally
-        {
-            Console.WriteLine("CommandHandler stopped.");
-        }
-    }
-
-    public async Task HandleQueue()
-    {
-        foreach (var (tcs, cmd) in _queue.Dequeue())
-        {
-            try
-            {
-                await Handle(cmd);
-                tcs.SetResult();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception occured procesing {cmd} - {ex.Message}");
-                tcs.SetException(ex);
-                await Task.Delay(EXCEPTION_DELAY);
-            }
-        }
-    }
-
-    private async Task Handle(Command cmd)
-    {
-        var (aggregateType, e) = cmd switch
-        {
-            AddCustomer c => ("customer", c.ToEvent()),
-            _ => throw new NotImplementedException($"Unknown command {cmd}")
-        };
-        await _eventStore.Append(aggregateType, e);
-    }
 }
