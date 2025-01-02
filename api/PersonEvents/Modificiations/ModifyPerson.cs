c65f68ce5b329bc0db2b690ef409e267ae53c610
Mohamed Elmalky <mohamed.elmalky@kaizen.io>
2025-01-02 15:20:47 -0600

    load person by events in modify

--- api/PersonEvents/Modificiations/ModifyPerson.cs
+++ api/PersonEvents/Modificiations/ModifyPerson.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel.Design;
 using Models;
+using Persistence;
 using Shared;
 
 namespace PersonEvents;
 
-public class ModifyPerson
+public class ModifyPerson(IEventStore eventStore)
 {
     public async Task Execute(PersonRequest request)
     {
+        if (string.IsNullOrWhiteSpace(request.Id))
+            throw new ArgumentException("Id is required");
+
+        var events = await eventStore.Load(request.Id);
+        if (events.Count == 0)
+            throw new ArgumentException("Person not found");
+        
+        var projection = new PersonProjection();
+        projection.Load(events);
+        var person = projection.GetPersonById(request.Id);
         
     }
 }
